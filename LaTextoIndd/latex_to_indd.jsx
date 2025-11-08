d6a94124bb775faad043516779ed06e0e3cb45bf
NeonHermit <velibor.stanisic@tutanota.com>
2025-11-08 12:07:32 +0100

    format date

--- LaTextoIndd/latex_to_indd.jsx
+++ LaTextoIndd/latex_to_indd.jsx
@@ -45,8 +45,17 @@ if (!texSubDir.exists) {
 }
 
 // Generate unique file name based on current timestamp
-var timestamp = (new Date()).getTime();
-var latexFileName = "latexInput_" + timestamp + ".tex";
+var now = new Date();
+var year = now.getFullYear();
+var month = ("0" + (now.getMonth() + 1)).slice(-2);
+var day = ("0" + now.getDate()).slice(-2);
+var hour = ("0" + now.getHours()).slice(-2);
+var minute = ("0" + now.getMinutes()).slice(-2);
+var second = ("0" + now.getSeconds()).slice(-2);
+
+var formattedTime = year + "-" + month + "-" + day + "-" + hour + "-" + minute + "-" + second;
+var latexFileName = "latexInput_" + formattedTime + ".tex";
+
 var latexFile = File(texSubDir + "/" + latexFileName);
 
 latexFile.encoding = "UTF8";
